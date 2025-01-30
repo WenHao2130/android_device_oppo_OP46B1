66949943ceced3511030c4f48e9492e4466ca6e8
WenHao2130 <WenHao2130@outlook.com>
2025-01-30 12:27:55 +0800

    OP46B1: Make `BOARD_KERNEL_CMDLINE` more beautiful
    Signed-off-by: WenHao2130 <WenHao2130@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,7 +41,23 @@ BOARD_USES_QCOM_FBE_DECRYPTION := true
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 1
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA90000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 androidboot.configfs=true androidboot.usbcontroller=a600000.dwc3 swiotlb=1 loop.max_part=7 printk.devkmsg=on kpti=off buildvariant=user
+BOARD_KERNEL_CMDLINE := \
+	console=ttyMSM0,115200n8 \
+	earlycon=msm_geni_serial,0xA90000 \
+	androidboot.hardware=qcom \
+	androidboot.console=ttyMSM0 \
+	video=vfb:640x400,bpp=32,memsize=3072000 \
+	msm_rtb.filter=0x237 \
+	ehci-hcd.park=3 \
+	lpm_levels.sleep_disabled=1 \
+	service_locator.enable=1 \
+	androidboot.configfs=true \
+	androidboot.usbcontroller=a600000.dwc3 \
+	swiotlb=1 \
+	loop.max_part=7 \
+	printk.devkmsg=on \
+	kpti=off \
+	buildvariant=user
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
